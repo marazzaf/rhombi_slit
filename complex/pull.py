3cc356af31278eecc5ed1168102e07fc03ddef71
marazzaf <frederic.marazzato@ponts.org>
2023-03-28 11:18:47 -0500

    Did a computation with the hyperbolic and the elliptic system. Gives very different results.

--- complex/pull.py
+++ complex/pull.py
@@ -2,11 +2,8 @@ import dolfinx
 from mpi4py import MPI
 import numpy as np
 LL,H = 16,16
-mesh = dolfinx.mesh.create_rectangle(MPI.COMM_WORLD, [[0,0], [LL,H]], [10, 10])
+mesh = dolfinx.mesh.create_rectangle(MPI.COMM_WORLD, [[0,0], [LL,H]], [50, 50])
 V = dolfinx.fem.FunctionSpace(mesh, ("Lagrange", 1))
-u_c = dolfinx.fem.Function(V, dtype=np.complex128)
-u_c.interpolate(lambda x:0.5 * (1 - x[1]/H) )
-print(u_c.x.array.dtype)
 
 from petsc4py import PETSc
 print(PETSc.ScalarType)
@@ -26,19 +23,26 @@ Gamma21 = mu2_p / mu1
 Gamma = ufl.as_tensor(((-Gamma21, 0.), (0., Gamma12)))
 delta = 1e-2
 Gamma += ufl.as_tensor(((delta*1j, 0), (0, delta*1j)))
+#test
+Gamma = ufl.as_tensor(((Gamma21, 0.), (0., Gamma12)))
 
 #Bilinear form
 v = ufl.TestFunction(V)
 a = ufl.inner(ufl.dot(Gamma, ufl.grad(uu)), ufl.grad(v)) * ufl.dx
 
 #Boundary conditions
+u_bc = dolfinx.fem.Function(V, dtype=np.complex128)
+val = 0.15
+#xi = lambda x: val * x[1]/H*2 if np.where(x[1] < H/2) else val * (2 - x[1]/H*2)
+xi = lambda x: -4*val/H**2 * x[1] * (x[1] - H)
+u_bc.interpolate(xi)
 mesh.topology.create_connectivity(mesh.topology.dim-1, mesh.topology.dim)
 boundary_facets = dolfinx.mesh.exterior_facet_indices(mesh.topology)
-dofs_L = dolfinx.fem.locate_dofs_geometrical(V, lambda x: np.isclose(x[1], 0))
-#boundary_dofs = dolfinx.fem.locate_dofs_topological(V, mesh.topology.dim-1, boundary_facets)
-bc1 = dolfinx.fem.dirichletbc(u_c, dofs_L) #boundary_dofs)
-dofs_R = dolfinx.fem.locate_dofs_geometrical(V, lambda x: np.isclose(x[1], LL))
-bc2 = dolfinx.fem.dirichletbc(u_c, dofs_R)
+tol = 1
+dofs_L = dolfinx.fem.locate_dofs_geometrical(V, lambda x: np.isclose(x[0], 0))
+bc1 = dolfinx.fem.dirichletbc(u_bc, dofs_L) #boundary_dofs)
+dofs_R = dolfinx.fem.locate_dofs_geometrical(V, lambda x: np.isclose(x[0], LL))
+bc2 = dolfinx.fem.dirichletbc(u_bc, dofs_R)
 
 #Nonlinear problem
 problem = dolfinx.fem.petsc.NonlinearProblem(a, uu, bcs=[bc1,bc2])
@@ -51,7 +55,7 @@ n, converged = solver.solve(uu)
 assert(converged)
 print(f"Number of interations: {n:d}")
 
-with dolfinx.io.XDMFFile(mesh.comm, "res.xdmf", "w") as xdmf:
+with dolfinx.io.XDMFFile(mesh.comm, "res_2.xdmf", "w") as xdmf:
     xdmf.write_mesh(mesh)
     uu.name = "Rotation"
     xdmf.write_function(uu)
