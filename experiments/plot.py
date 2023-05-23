4d3f478d835e884e6fca9aca5e72bc4d992f093c
marazzato <marazzato@system76-pc.localdomain>
2023-05-23 16:15:10 -0500

    Found a way to interpolate exp results. Also found a way to extract the results on the boundary of the domain. How can I use that for BC in the reference configuration?

--- experiments/plot.py
+++ experiments/plot.py
@@ -1,10 +1,52 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import sys
 
-data = np.loadtxt('test.txt')
+#Load data
+data = np.loadtxt('test.txt', comments='#')
 
-plt.scatter(data[:,0], data[:,1], c=data[:,2])
+#Plot data
+#plt.scatter(data[:,0], data[:,1], c=data[:,2])
+#plt.colorbar()
+#plt.show()
+#sys.exit()
+
+#Computing the convex Hull
+from scipy.spatial import ConvexHull
+points = data[:,:2]
+hull = ConvexHull(points)
+
+##Plotting the Hull
+#plt.plot(points[:,0], points[:,1], 'o')
+#for simplex in hull.simplices:
+#    plt.plot(points[simplex, 0], points[simplex, 1], 'k-')
+#plt.plot(points[hull.vertices,0], points[hull.vertices,1], 'r--', lw=2)
+#plt.plot(points[hull.vertices[0],0], points[hull.vertices[0],1], 'ro')
+#plt.show()
+
+#Creating the Linear interpolation
+from scipy.interpolate import LinearNDInterpolator
+interp = LinearNDInterpolator(data[:,:2], data[:,2])
+
+#Getting the BC
+x = points[hull.vertices,0]
+y = points[hull.vertices,1]
+res = np.array([x,y, interp(x,y)]) 
+print(res.shape)
+sys.exit()
+
+#Plotting the linear interpolation
+x = data[:,0]
+X = np.linspace(min(x), max(x))
+y = data[:,1]
+Y = np.linspace(min(y), max(y))
+X, Y = np.meshgrid(X, Y)
+Z = interp(X, Y)
+plt.pcolormesh(X, Y, Z, shading='auto')
+#plt.plot(x, y, "ok", label="input point")
+plt.legend()
 plt.colorbar()
+plt.axis("equal")
 plt.show()
 
 
